b7e369504c31a4055d59c1d4ad56f7aaf6d40ed0
exerinity <me@ex3.lol>
2026-02-26 17:54:26 +1100

    m

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const MIN_TTL_MINUTES = 1;
+export const MAX_TTL_MINUTES = 4320;
+export const SLUG_LENGTH = 6;
+export const SLUG_ATTEMPTS = 5;
+export const SLUG_KEY_PREFIX = 'slug:';
+export const IP_KEY_PREFIX = 'ip:';
+export const BLACKLIST_KEY_PREFIX = 'bl:';
